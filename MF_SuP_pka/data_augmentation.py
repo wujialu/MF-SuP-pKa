e05307da789e1201a02d6a318a2fe4b4ed57ce5e
wujialu <81202232+wujialu@users.noreply.github.com>
2022-11-08 14:55:31 +0800

    main

--- MF_SuP_pka/data_augmentation.py
+++ MF_SuP_pka/data_augmentation.py
@@ -1,5 +1,6 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
+import pandas as pd
 from MF_SuP_pka.ionization_group import get_ionization_aid
 
 def acid_to_base(smiles):
@@ -27,3 +28,22 @@ def base_to_acid(smiles):
         atom.SetNumExplicitHs(atom.GetTotalNumHs() + 1)
         b2a_smiles.append(Chem.MolToSmiles(mol))
     return b2a_smiles
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, default='../data/origin_data/chembl_pka_acidic_62w.csv')
+    parser.add_argument("--type", type=str, default='acid')
+    args = parser.parse_args()
+
+    data = pd.read_csv(args.data_path)
+    smiles = data.smiles.tolist()
+    if args.type == 'base':
+        b2a_smiles = base_to_acid(smiles)
+        data['conjugate_acid'] = b2a_smiles
+        data.to_csv(f'augmented_{args.data_path}', index=False)
+    elif args.type == 'acid':
+        a2b_smiles = acid_to_base(smiles)
+        data['conjugate_base'] = a2b_smiles
+        data.to_csv(f'augmented_{args.data_path}', index=False)
+    
